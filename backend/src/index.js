77dab9a46c0a38d44e08053b2e28554ff33009b8
Ture <petarturanovic00@gmail.com>
2026-02-04 14:46:01 +0100

    finished configuration for Pismonosha

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,9 +7,13 @@ import { connectDB } from "./lib/db.js";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
 
+import path from "path";
+
 dotenv.config();
 
 const PORT = process.env.PORT;
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -22,6 +26,13 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 server.listen(PORT, () => {
   console.log("Server is running on PORT:" + PORT);
   connectDB();
